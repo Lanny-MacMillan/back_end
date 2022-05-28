d6ec127bc39204d155bafa0f5eccce79eadfa49a
Lanny Mac <Lanny@MacBook-Pro.local>
2022-05-27 22:21:56 -0400

    testing .env

--- server.js
+++ server.js
@@ -22,8 +22,12 @@ const MONGODB_URI = process.env.MONGODB_URI;
 // Connect to Mongo &
 // Fix Depreciation Warnings from Mongoose
 // May or may not need these depending on your Mongoose version
-mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
-);
+// mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+// );
+mongoose.connect(MONGODB_URI, () => {
+  console.log('connected to mongo')
+});
+
 
 // Error / success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
