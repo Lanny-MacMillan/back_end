b6998e12fb16a22d129c5192799196d9cbcbcfe7
kbrpronet <kaibrown2889@gmail.com>
2022-06-03 14:34:03 -0400

    removed two lines

--- server.js
+++ server.js
@@ -41,9 +41,7 @@ db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
-//mongoose
-// mongoose.connect(MONGODB_URI, () => {
-// })
+
 mongoose.connection.once('open', ()=>{
     console.log('connected to mongod!');
 });
